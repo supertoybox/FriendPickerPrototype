9e58cd658a14fb9ab7b076f6c87179e26ad473bf
Andrew Vergara <drewvergara@gmail.com>
2012-12-25 12:04:14 -0800

    Initial Commit

---
+++ FriendPickerPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FriendPickerPrototype
+//
+//  Created by Andrew Vergara on 12/25/12.
+//  Copyright (c) 2012 Super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FriendPickerPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FriendPickerPrototype
+//
+//  Created by Andrew Vergara on 12/25/12.
+//  Copyright (c) 2012 Super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
